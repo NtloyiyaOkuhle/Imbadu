db12c953ede902f69edd2cd46d66f1dd21be530f
Okuhle <blessing.ntloyiya@gmail.com>
2022-08-26 12:21:38 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,32 +1,24 @@
 import time, socket, sys
  
-new_socket = socket.socket()
-host_name = socket.gethostname()
-s_ip = socket.gethostbyname(host_name)
+socket_server = socket.socket()
+server_host = socket.gethostname()
+ip = socket.gethostbyname(server_host)
+sport = 8080
  
-port = 8080
- 
-new_socket.bind((host_name, port))
-print( "Binding successful!”)
-print("This is your IP: ", s_ip)
- 
-name = input('Enter name: ')
- 
-new_socket.listen(1) 
+print('This is your IP address: ',ip)
+server_host = input('Enter friend\'s IP address:')
+name = input('Enter Friend\'s name: ')
  
  
-conn, add = new_socket.accept()
+socket_server.connect((server_host, sport))
  
-print("Received connection from ", add[0])
-print('Connection Established. Connected From: ',add[0])
+socket_server.send(name.encode())
+server_name = socket_server.recv(1024)
+server_name = server_name.decode()
  
-client = (conn.recv(1024)).decode()
-print(client + ' has connected.')
- 
-conn.send(name.encode())
+print(server_name,' has joined...')
 while True:
-    message = input('Me : ')
-    conn.send(message.encode())
-    message = conn.recv(1024)
-    message = message.decode()
-    print(client, ':', message)
+    message = (socket_server.recv(1024)).decode()
+    print(server_name, ":", message)
+    message = input("Me : ")
+    socket_server.send(message.encode())  
